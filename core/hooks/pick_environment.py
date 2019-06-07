65e8c108efecdcd138007f9f53371e3d36f0f91e
Kevin Sallee <kevin.sallee@sano.co>
2019-06-07 14:58:04 -0400

    addressed PR comments.

--- core/hooks/pick_environment.py
+++ core/hooks/pick_environment.py
@@ -46,7 +46,6 @@ class PickEnvironment(Hook):
                 self.logger.debug("Using site_vendor context")
                 return "site_vendor"
 
-            self.logger.info("Using vendor config")
             self.logger.debug("Using vendor context")
             return "vendor"
 
@@ -56,11 +55,9 @@ class PickEnvironment(Hook):
             if context.project is None:
                 # this happens when Desktop boots against a project-independent config
                 # but no project has been chosen yet
-                self.logger.info("Using site_vendor config (dev)")
                 self.logger.debug("Using site_vendor context")
                 return "site_vendor"
 
-            self.logger.info("Using vendor config (dev)")
             self.logger.debug("Using vendor context")
             return "vendor"
 
